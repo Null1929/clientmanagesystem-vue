78262f7a048557e0d64a7960f17f966ddb64dd42
Chrissy <Chrissy@yjg.com>
2023-12-19 16:25:04 +0800

    完成客户构成分析的标准化改造，同时注意，因为使用RPC调用会使分页失效，因此这个接口被写在了ClientServer的ClientLostController中

--- src/components/pages/statisticalAnalysis-service/pageClientChurnAnalysis.vue
+++ src/components/pages/statisticalAnalysis-service/pageClientChurnAnalysis.vue
@@ -1,208 +1,146 @@
 <template>
-    <div>
-        <div class="search" style="width: 55vw;">
-            客户名称
-            <input type="text" v-model="client.clientName" style="margin-right: 2rem;" />
-            客户经理
-            <input type="text" v-model="client.clientManager" style="margin-right: 3rem;" />
-            <el-button @click="query()">查询</el-button>
-        </div>
+  <table-style>
 
-        <table style="width:50vw">
-            <tr>
-                <td>编号</td>
-                <td>年份</td>
-                <td>客户</td>
-                <td>客户经理</td>
-                <td>流失原因</td>
-            </tr>
+    <template #header>
+      <label>客户名称:</label> <input type="text" v-model="client.clientName"/>
+      &nbsp;&nbsp;&nbsp;&nbsp;
+      <label>客户经理:</label> <input type="text" v-model="client.clientManager"/>
+      &nbsp;&nbsp;
+      <el-button @click="query()">查询</el-button>
+    </template>
 
-            <tr v-for="item in pageResult.data" :key="item.clientId">
-                <td>{{ item.clientId }}</td>
-                <td>{{ item.lostTime.substring(0, 4) }}</td>
-                <td>{{ item.clientName }}</td>
-                <td>{{ item.clientManager }}</td>
-                <td>{{ item.lossReason }}</td>
-            </tr>
-        </table>
-        <table>
-            <tr>
-                <td>共有{{ pageResult.allData }}条记录</td>
-                <td>第{{ pageResult.nowPage }}/共{{ pageResult.allPages }}页</td>
-                <td>
-                    <el-button @click="firstPage()" id="firstPage">第一页</el-button>
-                    <el-button type="primary" icon="el-icon-arrow-left" @click="lastPage()" id="lastPage">上一页</el-button>
-                    <el-button type="primary" @click="nextPage()" id="nextPage">下一页
-                        <i class="el-icon-arrow-right el-icon--right"></i>
-                    </el-button>
-                    <el-button @click="endPage()" id="endPage">最后一页</el-button>
-                </td>
-                <td>
-                    转到<input type="text" v-model="forw" />页
-                    <el-button @click="forward()">Go</el-button>
-                </td>
-            </tr>
-        </table>
-    </div>
+
+    <table style="width:50vw">
+      <tr>
+        <td>编号</td>
+        <td>年份</td>
+        <td>客户</td>
+        <td>客户经理</td>
+        <td>流失原因</td>
+      </tr>
+
+      <tr v-for="item in pageResult.result" :key="item.clientId">
+        <td>{{ item.clientId }}</td>
+        <td>{{ item.lostTime.substring(0, 4) }}</td>
+        <td>{{ item.clientName }}</td>
+        <td>{{ item.clientManager }}</td>
+        <td>{{ item.lostReason }}</td>
+      </tr>
+    </table>
+    <template #footer>
+      <table>
+        <tr>
+          <td>共有{{ pageResult.total }}条记录</td>
+          <td>第{{ pageResult.pageNum }}页 共{{ Math.ceil(pageResult.total / pageResult.pageSize) }}页</td>
+          <td>
+            <el-button @click="firstPage()" id="firstPage">第一页</el-button>
+          </td>
+          <td>
+            <el-button @click="lastPage()" id="lastPage">上一页</el-button>
+          </td>
+          <td>
+            <el-button @click="nextPage()" id="nextPage">下一页</el-button>
+          </td>
+          <td>
+            <el-button @click="endPage()" id="endPage">最后一页</el-button>
+          </td>
+          <td>转到<input type="text" v-model="pageResult.forward">页
+            <el-button @click="forward()">Go</el-button>
+          </td>
+        </tr>
+      </table>
+    </template>
+  </table-style>
 </template>
 
 <script>
 import httpRequest from "@/request";
+import TableStyle from "@/components/slot/tableStyle";
+
 export default {
-    name: "ClientmanagesystemPageClientChurnAnalysis",
+  name: "ClientmanagesystemPageClientChurnAnalysis",
+  components: {TableStyle},
+  data() {
+    return {
+      client: {
+        clientName: null,
+        clientManager: null,
+      },
 
-    data() {
-        return {
-            client: {
-                clientName: null,
-                clientManager: null,
+      pageResult: {
+        total: 0,
+        pageNum: 1,
+        pageSize: 10,
+        forward: 1,
+        result: []
+      },
+    };
+  },
+
+  mounted() {
+    this.query();
+  },
+
+  methods: {
+    query() {
+      httpRequest
+          .get("/clientservice/clientLost/queryClientChurn", {
+            params: {
+              clientName: this.client.clientName,
+              clientManager: this.client.clientManager,
+              pageNum: this.pageResult.pageNum,
+              pageSize: this.pageResult.pageSize,
             },
-
-            pageResult: {
-                allData: null,
-                allPages: null,
-                nowPage: null,
-                firstPage: null,
-                endPage: null,
-                pageSize: null,
-                data: [],
-            },
-
-            forw: 1,
-        };
+          })
+          .then((response) => {
+            if (response.data.resCode === "000000") {
+              this.pageResult = response.data.data;
+            }
+          });
     },
 
-    mounted() {
-        httpRequest
-            .get("/statisticalanalysisservice/statisticalAnalysis/queryClientChurn", {
-                params: {
-                    clientName: this.client.clientName,
-                    clientManager: this.client.clientManager,
-                    pageNum: 1,
-                    pageSize: 10,
-                },
-            })
-            .then((response) => {
-                this.pageResult = response.data.data;
-            })
-            .catch((err) => { });
+    /*******************************************/
+    /**
+     * 分页方法
+     */
+    firstPage() {
+      this.pageResult.pageNum = 1;
+      this.query();
     },
-
-    methods: {
-        query() {
-            httpRequest
-                .get("/statisticalanalysisservice/statisticalAnalysis/queryClientChurn", {
-                    params: {
-                        clientName: this.client.clientName,
-                        clientManager: this.client.clientManager,
-                        pageNum: 1,
-                        pageSize: this.pageResult.pageSize,
-                    },
-                })
-                .then((response) => {
-                    this.pageResult = response.data.data;
-                })
-                .catch((err) => { });
-        },
-
-        firstPage() {
-            if (this.pageResult.nowPage != 1) {
-                httpRequest
-                    .get("/statisticalanalysisservice/statisticalAnalysis/queryClientChurn", {
-                        params: {
-                            clientName: null,
-                            clientManager: null,
-                            pageNum: 1,
-                            pageSize: this.pageResult.pageSize,
-                        },
-                    })
-                    .then((response) => {
-                        this.pageResult = response.data.data;
-                    })
-                    .catch((err) => { });
-            }
-        },
-
-        lastPage() {
-            if (this.pageResult.nowPage != 1) {
-                httpRequest
-                    .get("/statisticalanalysisservice/statisticalAnalysis/queryClientChurn", {
-                        params: {
-                            clientName: null,
-                            clientManager: null,
-                            pageNum: this.pageResult.nowPage - 1,
-                            pageSize: this.pageResult.pageSize,
-                        },
-                    })
-                    .then((response) => {
-                        this.pageResult = response.data.data;
-                    })
-                    .catch((err) => { });
-            }
-        },
-
-        nextPage() {
-            if (this.pageResult.nowPage != this.pageResult.endPage) {
-                httpRequest
-                    .get("/statisticalanalysisservice/statisticalAnalysis/queryClientChurn", {
-                        params: {
-                            clientName: null,
-                            clientManager: null,
-                            pageNum: this.pageResult.nowPage + 1,
-                            pageSize: this.pageResult.pageSize,
-                        },
-                    })
-                    .then((response) => {
-                        this.pageResult = response.data.data;
-                    })
-                    .catch((err) => { });
-            }
-        },
-        endPage() {
-            if (this.pageResult.nowPage != this.pageResult.endPage)
-                httpRequest
-                    .get("/statisticalanalysisservice/statisticalAnalysis/queryClientChurn", {
-                        params: {
-                            clientName: null,
-                            clientManager: null,
-                            pageNum: this.pageResult.endPage,
-                            pageSize: this.pageResult.pageSize,
-                        },
-                    })
-                    .then((response) => {
-                        this.pageResult = response.data.data;
-                    })
-                    .catch((err) => { });
-        },
-        forward() {
-            if (
-                this.forw < this.pageResult.firstPage ||
-                this.forw > this.pageResult.endPage
-            ) {
-                alert("请输入有效页数");
-            } else {
-                httpRequest
-                    .get("/statisticalanalysisservice/statisticalAnalysis/queryClientChurn", {
-                        params: {
-                            clientName: null,
-                            clientManager: null,
-                            pageNum: this.forw,
-                            pageSize: this.pageResult.pageSize,
-                        },
-                    })
-                    .then((response) => {
-                        this.pageResult = response.data.data;
-                    })
-                    .catch((err) => { });
-            }
-        },
+    lastPage() {
+      if (this.pageResult.pageNum != 1) {
+        --this.pageResult.pageNum
+        this.query();
+      }
     },
+    nextPage() {
+      if (this.pageResult.pageNum != Math.ceil(this.pageResult.total / this.pageResult.pageSize)) {
+        ++this.pageResult.pageNum
+        this.query();
+      }
+    },
+    endPage() {
+      this.pageResult.pageNum = Math.ceil(this.pageResult.total / this.pageResult.pageSize);
+      this.query();
+    },
+    forward() {
+      if (this.pageResult.forward >= 1 && this.pageResult.forward <= Math.ceil(this.pageResult.total / this.pageResult.pageSize)) {
+        this.pageResult.pageNum = this.pageResult.forward;
+        this.query();
+      } else {
+        alert("页数不正确！")
+        this.pageResult.forward = null;
+      }
+    },
+    /*******************************************/
+
+  },
 };
 </script>
 <style lang="less" scoped>
 .main {
-    padding: 0 !important;
-    margin: 0 !important;
+  padding: 0 !important;
+  margin: 0 !important;
 
 }
 </style>
